38d370e3595c5ffb912b0bda4968a43e4bce1834
Amina Said Ibrahim <amina7585@gmail.com>
2025-03-25 20:45:19 +0000

    sign in page done and styled

---
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return (
+    <main className="h-[80vh] w-full bg-gradient-to-r from-[#A9B4C2] to-[#EEF1EF] text-[#496c78] text-center flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-semibold mb-4 drop-shadow-lg">
+        Welcome to The Card Generator!
+      </h1>
+      <h2>Sign in to continue</h2>
+      <SignIn />
+    </main>
+  );
+}
